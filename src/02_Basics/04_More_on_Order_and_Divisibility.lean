85a503b0387439b73ea58941d24c1a527322fbb6
kvanvels <kvanvels@protonmail.com>
2022-08-19 19:06:52 -0400

    more

--- src/02_Basics/04_More_on_Order_and_Divisibility.lean
+++ src/02_Basics/04_More_on_Order_and_Divisibility.lean
@@ -40,21 +40,72 @@ begin
 end
 
 example : max a b = max b a :=
-sorry
+begin
+  apply le_antisymm,
+  repeat{
+    apply max_le,
+    apply le_max_right,
+    apply le_max_left,  },
+end
 
 example : min (min a b) c = min a (min b c) :=
-sorry
+begin
+  apply le_antisymm,  
+  have h1 := min_le_left (min a b) c,
+  apply le_min,
+  
+  have h2 := min_le_left a b,
+  exact le_trans h1 h2,
+  apply le_min,  
+  have h2 := min_le_right a b,
+  exact le_trans h1 h2,
+  exact min_le_right (min a b) c,
+  have h1 := min_le_right a (min b c),
+  apply le_min,
+  apply le_min,
+  exact min_le_left a (min b c),  
+  have h2 := min_le_left b c,
+  exact le_trans h1 h2,
+  have h2 := min_le_right b c,
+  exact le_trans h1 h2,
+  end
 
 lemma aux : min a b + c ≤ min (a + c) (b + c) :=
-sorry
-
+begin
+  apply le_min,
+  apply add_le_add,
+  apply min_le_left,
+  refl,
+  apply add_le_add,
+  apply min_le_right,
+  refl,
+end
 example : min a b + c = min (a + c) (b + c) :=
-sorry
+begin
+  apply le_antisymm,
+  apply aux,
+  rw ←sub_add_cancel (min (a + c) (b + c)) c,
+  apply add_le_add_right,
+  have h1 := aux (a+c) (b+c) (-c),
+  rw [add_neg_cancel_right, add_neg_cancel_right] at h1,
+  rw (  tactic.ring.add_neg_eq_sub _ c )at h1,
+  exact h1,
+end
+
+
+
 
 #check (abs_add : ∀ a b : ℝ, abs (a + b) ≤ abs a + abs b)
 
+--example (a b : ℝ) : (a + -b) =( a- b) := by library_search,
+
 example : abs a - abs b ≤ abs (a - b) :=
-sorry
+begin
+  have h1 : abs a ≤ abs (a -b) + abs b := 
+  calc 
+    abs a = abs ((a -b) + b)      :  by simp
+      ... ≤ abs (a - b) + abs (b) : by exact (abs_add (a-b) b),  
+  linarith,
 
 end
 
@@ -74,7 +125,19 @@ example : x ∣ x^2 :=
 by apply dvd_mul_right
 
 example (h : x ∣ w) : x ∣ y * (x * z) + x^2 + w^2 :=
-sorry
+begin
+  apply dvd_add,
+  apply dvd_add,
+  apply dvd_mul_of_dvd_right,
+  apply dvd_mul_of_dvd_left,
+  exact dvd_rfl,
+  rw pow_two x,
+  apply dvd_mul_of_dvd_left,
+  exact dvd_rfl,
+  rw pow_two w,
+  apply dvd_mul_of_dvd_left,
+  exact h,
+end
 
 end
 
@@ -87,7 +150,18 @@ open nat
 #check (lcm_zero_right n : lcm n 0 = 0)
 #check (lcm_zero_left n  : lcm 0 n = 0)
 
-example : gcd m n = gcd n m :=
-sorry
 
+
+example : gcd m n = gcd n m :=
+begin
+  apply dvd_antisymm,
+  apply dvd_gcd,
+  apply gcd_dvd_right,
+  apply gcd_dvd_left,
+  apply dvd_gcd,
+  apply gcd_dvd_right,
+  apply gcd_dvd_left,
+end
+
+end
 end
