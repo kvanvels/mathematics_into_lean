b967c63db98c7ad69f4b31684e2072d6499c0c12
kvanvels <kvanvels@protonmail.com>
2022-08-20 11:08:32 -0400

    Implications and the universal

--- src/03_Logic/01_Implication_and_the_Universal_Quantifier.lean
+++ src/03_Logic/01_Implication_and_the_Universal_Quantifier.lean
@@ -1,12 +1,35 @@
 import data.real.basic
 
+
 #check ∀ x : ℝ, 0 ≤ x → abs x = x
 
 #check ∀ x y ε : ℝ, 0 < ε → ε ≤ 1 → abs x < ε → abs y < ε → abs (x * y) < ε
 
 lemma my_lemma : ∀ x y ε : ℝ,
   0 < ε → ε ≤ 1 → abs x < ε → abs y < ε → abs (x * y) < ε :=
-sorry
+begin
+  intros x y ε ε_pos ε_le1 h1 h2,
+  apply or.elim (em (x = 0)),
+  intro x0,
+  rw [x0,zero_mul,abs_zero],
+  exact ε_pos,
+  intro x_pos,
+  apply or.elim (em (y = 0)),
+  intro y0,
+  rw [y0,mul_zero,abs_zero],
+  exact ε_pos,
+  intro y_pos,
+  rw abs_mul,
+  rw ←mul_one ε,
+  apply mul_lt_mul,
+  exact h1,
+  apply le_trans,
+  exact h2.le,
+  exact ε_le1,
+  rw abs_pos,
+  exact y_pos,
+  exact ε_pos.le,
+end
 
 section
   variables a b δ : ℝ
@@ -18,35 +41,7 @@ section
   #check my_lemma a b δ h₀ h₁ ha hb
 end
 
-lemma my_lemma2 : ∀ {x y ε : ℝ},
-  0 < ε → ε ≤ 1 → abs x < ε → abs y < ε → abs (x * y) < ε :=
-sorry
 
-section
-  variables a b δ : ℝ
-  variables (h₀ : 0 < δ) (h₁ : δ ≤ 1)
-  variables (ha : abs a < δ) (hb : abs b < δ)
-
-  #check my_lemma2 h₀ h₁ ha hb
-end
-
-lemma my_lemma3 : ∀ {x y ε : ℝ},
-  0 < ε → ε ≤ 1 → abs x < ε → abs y < ε → abs (x * y) < ε :=
-begin
-  intros x y ε epos ele1 xlt ylt,
-  sorry
-end
-
-lemma my_lemma4 : ∀ {x y ε : ℝ},
-  0 < ε → ε ≤ 1 → abs x < ε → abs y < ε → abs (x * y) < ε :=
-begin
-  intros x y ε epos ele1 xlt ylt,
-  calc
-    abs (x * y) = abs x * abs y : sorry
-    ... ≤ abs x * ε             : sorry
-    ... < 1 * ε                 : sorry
-    ... = ε                     : sorry
-end
 
 def fn_ub (f : ℝ → ℝ) (a : ℝ) : Prop := ∀ x, f x ≤ a
 def fn_lb (f : ℝ → ℝ) (a : ℝ) : Prop := ∀ x, a ≤ f x
@@ -70,12 +65,35 @@ sorry
 
 example (nnf : fn_lb f 0) (nng : fn_lb g 0) :
   fn_lb (λ x, f x * g x) 0 :=
-sorry
+begin
+  intro x,
+  have h1 := nnf x,
+  have h2 := nng x,
+  dsimp,
+  rw ←mul_zero (0: ℝ),
+  apply mul_le_mul,
+  exact h1,
+  exact h2,
+  refl,
+  exact h1,  
+  
+end
 
 example (hfa : fn_ub f a) (hfb : fn_ub g b)
     (nng : fn_lb g 0) (nna : 0 ≤ a) :
   fn_ub (λ x, f x * g x) (a * b) :=
-sorry
+begin
+  intro x,
+  dsimp,
+  specialize hfa x,
+  specialize hfb x,
+  specialize nng x,
+  apply mul_le_mul,
+  exact hfa,
+  exact hfb,
+  exact nng,
+  exact nna,    
+end
 
 end
 
@@ -113,11 +131,22 @@ example (mf : monotone f) (mg : monotone g) :
 
 example {c : ℝ} (mf : monotone f) (nnc : 0 ≤ c) :
   monotone (λ x, c * f x) :=
-sorry
+begin
+  intros x1 x2 h1,
+  exact mul_le_mul_of_nonneg_left (mf h1) nnc,
+end
+
+
 
 example (mf : monotone f) (mg : monotone g) :
   monotone (λ x, f (g x)) :=
-sorry
+begin
+  intros x1 x2 h1,
+  dsimp,
+  apply mf,
+  apply mg h1,
+end
+
 
 def fn_even (f : ℝ → ℝ) : Prop := ∀ x, f x = f (-x)
 def fn_odd (f : ℝ → ℝ) : Prop := ∀ x, f x = - f (-x)
@@ -131,13 +160,38 @@ begin
 end
 
 example (of : fn_odd f) (og : fn_odd g) : fn_even (λ x, f x * g x) :=
-sorry
+begin
+  intros x1,
+  dsimp,
+  specialize of x1,
+  specialize og x1,
+  rw [of,og],
+  rw neg_mul_neg,  
+end
+
+
 
 example (ef : fn_even f) (og : fn_odd g) : fn_odd (λ x, f x * g x) :=
-sorry
+begin
+  intro x1,
+  dsimp,
+  specialize ef x1,
+  specialize og x1,
+  rw [ef,og],
+  exact mul_neg (f (-x1) ) (g (-x1)),
+end  
+
 
 example (ef : fn_even f) (og : fn_odd g) : fn_even (λ x, f (g x)) :=
-sorry
+begin
+  intro x1,
+  dsimp,
+  specialize og x1,
+  rw og,
+  specialize ef (g (-x1) ),
+  rw ef,  
+end
+
 
 end
 
@@ -150,7 +204,10 @@ by { intros x xs, exact xs }
 theorem subset.refl : s ⊆ s := λ x xs, xs
 
 theorem subset.trans : r ⊆ s → s ⊆ t → r ⊆ t :=
-sorry
+begin
+  intros h1 h2 x h3,
+  exact h2 (h1 h3),
+end
 
 end
 
@@ -162,7 +219,14 @@ variables (s : set α) (a b : α)
 def set_ub (s : set α) (a : α) := ∀ x, x ∈ s → x ≤ a
 
 example (h : set_ub s a) (h' : a ≤ b) : set_ub s b :=
-sorry
+begin
+  intros x h1,
+  specialize h x,
+  specialize h h1,
+  apply le_trans,
+  exact h,
+  exact h',  
+end
 
 end
 
@@ -176,13 +240,28 @@ begin
 end
 
 example {c : ℝ} (h : c ≠ 0) : injective (λ x, c * x) :=
-sorry
+begin
+  intros x1 x2,
+  dsimp,
+  intro h1,
+  rw ←(mul_right_inj' h),
+  exact h1,
+end
+
+
 
 variables {α : Type*} {β : Type*} {γ : Type*}
 variables {g : β → γ} {f : α → β}
 
 example (injg : injective g) (injf : injective f) :
   injective (λ x, g (f x)) :=
-sorry
+begin
+  intros x1 x2,
+  dsimp,
+  intro h1,
+  apply injf,
+  apply injg,
+  exact h1,  
+end
 
 end
