5e4f67df59062e60753d5cb2e9101d3bffc11441
kvanvels <kvanvels@protonmail.com>
2022-08-20 12:52:59 -0400

    conjuction-biamp

--- src/03_Logic/04_Conjunction_and_Bi-implication.lean
+++ src/03_Logic/04_Conjunction_and_Bi-implication.lean
@@ -49,7 +49,14 @@ example {x y : ℝ} (h : x ≤ y ∧ x ≠ y) : ¬ y ≤ x :=
 
 example {m n : ℕ} (h : m ∣ n ∧ m ≠ n) :
   m ∣ n ∧ ¬ n ∣ m :=
-sorry
+begin
+  apply and.intro,
+  exact h.left,
+  intro h1,
+  have h2 :=nat.dvd_antisymm h.left h1, 
+  exact h.right h2,
+end
+
 
 example : ∃ x : ℝ, 2 < x ∧ x < 4 :=
 ⟨5/2, by norm_num, by norm_num⟩
@@ -96,17 +103,48 @@ example {x y : ℝ} (h : x ≤ y) : ¬ y ≤ x ↔ x ≠ y :=
 ⟨λ h₀ h₁, h₀ (by rw h₁), λ h₀ h₁, h₀ (le_antisymm h h₁)⟩
 
 example {x y : ℝ} : x ≤ y ∧ ¬ y ≤ x ↔ x ≤ y ∧ x ≠ y :=
-sorry
+begin
+  apply iff.intro,
+  rintros ⟨h1,h2⟩,
+  apply and.intro h1,
+  intro h3,
+  rw h3 at h2,
+  apply h2,
+  refl,
+  rintros ⟨h1,h2⟩,
+  apply and.intro h1,
+  intro h3,
+  apply h2,
+  apply le_antisymm,
+  exact h1,
+  exact h3,  
+end
+
+example (a b : ℝ) (h : a ≥ 0) (h1 : b ≥ 0) (h2 : a + b = 0) : a = 0 := by 
 
 theorem aux {x y : ℝ} (h : x^2 + y^2 = 0) : x = 0 :=
 begin
   have h' : x^2 = 0,
-  { sorry },
+  {
+    have h1 := pow_two_nonneg x,
+    have h2 := pow_two_nonneg y,
+    linarith,
+  },
   exact pow_eq_zero h'
 end
 
 example (x y : ℝ) : x^2 + y^2 = 0 ↔ x = 0 ∧ y = 0 :=
-sorry
+begin
+  apply iff.intro,
+  intro h1,
+  apply and.intro,
+  exact aux h1,
+  rw add_comm at h1,
+  exact aux h1,
+  rintros ⟨h1,h2⟩,
+  rw [h1,h2],
+  ring_nf,
+end
 
 section
 
@@ -130,7 +168,15 @@ theorem not_monotone_iff {f : ℝ → ℝ}:
 by { rw monotone, push_neg }
 
 example : ¬ monotone (λ x : ℝ, -x) :=
-sorry
+begin
+  rw not_monotone_iff,
+  apply  exists.intro (0 : real),
+  apply exists.intro (1 : real),
+  apply and.intro,
+  norm_num,
+  norm_num,
+end
+
 
 section
 variables {α : Type*} [partial_order α]
@@ -139,7 +185,16 @@ variables a b : α
 example : a < b ↔ a ≤ b ∧ a ≠ b :=
 begin
   rw lt_iff_le_not_le,
-  sorry
+  apply iff.intro,
+  rintros ⟨h1,h2⟩,
+  apply and.intro h1,
+  intro h1,
+  apply h2,
+  rw h1,
+  rintros ⟨h1,h2⟩,
+  apply and.intro h1,
+  intro h3,
+  exact h2 (le_antisymm h1 h3),  
 end
 
 end
@@ -151,13 +206,27 @@ variables a b c : α
 example : ¬ a < a :=
 begin
   rw lt_iff_le_not_le,
-  sorry
+  rintros ⟨h1,h2⟩,
+  apply h2,
+  refl,  
 end
 
 example : a < b → b < c → a < c :=
 begin
   simp only [lt_iff_le_not_le],
-  sorry
+  rintros ⟨h1,h2⟩ ⟨h3,h4⟩,
+  apply and.intro,
+  apply le_trans,
+  exact h1,
+  exact h3,
+  intro h5,
+  have h6 : b ≤ a := 
+  begin
+    apply le_trans,
+    exact h3,
+    exact h5,
+  end,
+  exact h2 h6,
 end
 
 end
