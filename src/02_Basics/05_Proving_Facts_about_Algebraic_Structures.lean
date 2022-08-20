83d98058d5fffc52ec0cfcf54adeced32bbac0f9
kvanvels <kvanvels@protonmail.com>
2022-08-19 22:42:04 -0400

    something

--- src/02_Basics/05_Proving_Facts_about_Algebraic_Structures.lean
+++ src/02_Basics/05_Proving_Facts_about_Algebraic_Structures.lean
@@ -33,14 +33,92 @@ variables x y z : α
 #check (le_sup_right: y ≤ x ⊔ y)
 #check (sup_le : x ≤ z → y ≤ z → x ⊔ y ≤ z)
 
-example : x ⊓ y = y ⊓ x := sorry
-example : x ⊓ y ⊓ z = x ⊓ (y ⊓ z) := sorry
-example : x ⊔ y = y ⊔ x := sorry
-example : x ⊔ y ⊔ z = x ⊔ (y ⊔ z) := sorry
+example : x ⊓ y = y ⊓ x := 
+begin
+  apply le_antisymm,
+  apply le_inf,
+  apply inf_le_right,
+  apply inf_le_left,
+  apply le_inf,
+  apply inf_le_right,
+  apply inf_le_left,
+end
 
-theorem absorb1 : x ⊓ (x ⊔ y) = x := sorry
-theorem absorb2 : x ⊔ (x ⊓ y) = x := sorry
+theorem k1 : x ⊓ y ⊓ z = x ⊓ (y ⊓ z) :=
+begin
+  apply le_antisymm,  
+  apply le_inf,
+  apply le_trans,
+  apply inf_le_left,
+  apply inf_le_left,
+  apply le_inf,
+  apply le_trans,
+  apply inf_le_left,
+  apply inf_le_right,
+  apply inf_le_right,
+  apply le_inf,
+  apply le_inf,
+  apply inf_le_left,
+  apply le_trans,
+  apply inf_le_right,
+  apply inf_le_left,
+  apply le_trans,
+  apply inf_le_right,
+  apply inf_le_right,
+end
 
+
+
+example : x ⊔ y = y ⊔ x := 
+begin
+  apply le_antisymm,
+  apply sup_le,
+  apply le_sup_right,
+  apply le_sup_left,
+  apply sup_le,
+  apply le_sup_right,
+  apply le_sup_left,        
+end
+example : x ⊔ y ⊔ z = x ⊔ (y ⊔ z) := 
+begin
+  apply le_antisymm,
+  apply sup_le,
+  apply sup_le,
+  apply le_sup_left,
+  apply le_trans,
+  apply @le_sup_left _ _ y z,
+  apply le_sup_right,
+  apply le_trans,
+  apply @le_sup_right _ _ y z,
+  apply le_sup_right,
+  apply sup_le,
+  apply le_trans,
+  apply @le_sup_left _ _ x y,
+  apply le_sup_left,
+  apply sup_le,
+  apply le_trans,
+  apply @le_sup_right _ _ x y,
+  apply le_sup_left,
+  apply le_sup_right,
+end
+
+theorem absorb1 : x ⊓ (x ⊔ y) = x := 
+begin
+  apply le_antisymm,
+  apply inf_le_left,
+  apply le_inf,
+  refl,
+  apply le_sup_left,
+end
+theorem absorb2 : x ⊔ (x ⊓ y) = x := 
+begin
+  apply le_antisymm,
+  apply sup_le,
+  refl,
+  apply inf_le_left,
+  apply le_sup_left,
+  
+end  
 end
 
 section
@@ -60,11 +138,35 @@ variables a b c : α
 
 example (h : ∀ x y z : α, x ⊓ (y ⊔ z) = (x ⊓ y) ⊔ (x ⊓ z)) :
   a ⊔ (b ⊓ c) = (a ⊔ b) ⊓ (a ⊔ c) :=
-sorry
+begin
+ rw h,
+ rw @inf_comm _ _ (a ⊔ b), 
+ rw absorb1, 
+ rw @inf_comm _ _ (a ⊔ b), 
+ rw h,
+ rw ←sup_assoc, 
+ rw @inf_comm _ _ c a, 
+ rw absorb2, 
+ rw inf_comm,  
+end
 
 example (h : ∀ x y z : α, x ⊔ (y ⊓ z) = (x ⊔ y) ⊓ (x ⊔ z)) :
   a ⊓ (b ⊔ c) = (a ⊓ b) ⊔ (a ⊓ c) :=
-sorry
+begin
+rw h, 
+rw @sup_comm _ _ (a ⊓ b), 
+rw absorb2, 
+rw @sup_comm _ _ (a ⊓ b), 
+rw h,
+rw    ←inf_assoc, 
+rw@sup_comm _ _ c a, 
+rw absorb1, 
+rw sup_comm,
+  
+  
+  
+  
+end
 
 end
 
@@ -77,12 +179,34 @@ variables a b c : R
 
 #check (mul_nonneg : 0 ≤ a → 0 ≤ b → 0 ≤ a * b)
 
-example : a ≤ b → 0 ≤ b - a := sorry
+theorem aux1 : a ≤ b → 0 ≤ b - a := 
+begin
+  intro h,
+  rw [←sub_self a],
+  rw sub_eq_add_neg,
+  rw sub_eq_add_neg,
+  rw add_comm,
+  rw add_comm b,
+  apply add_le_add_left,
+  exact h,
+  
+end
 
-example : 0 ≤ b - a → a ≤ b := sorry
+theorem aux2 : 0 ≤ b - a → a ≤ b := 
+begin
+  intro h,
+  rw [←add_zero a, ←sub_add_cancel b a, add_comm (b-a)],
+  apply add_le_add_left,
+  exact h,
+end
 
-example (h : a ≤ b) (h' : 0 ≤ c) : a * c ≤ b * c := sorry
-
+example (h : a ≤ b) (h' : 0 ≤ c) : a * c ≤ b * c := 
+begin
+  have h1 : 0 ≤ (b -a) * c,
+  { exact mul_nonneg (aux1 _ _ h) h'},
+  rw sub_mul at h1,
+  exact aux2 _ _ h1,
+end  
 end
 
 section
@@ -93,7 +217,15 @@ variables x y z : X
 #check (dist_comm x y : dist x y = dist y x)
 #check (dist_triangle x y z : dist x z ≤ dist x y + dist y z)
 
-example (x y : X) : 0 ≤ dist x y := sorry
+
+example (x y : X) : 0 ≤ dist x y :=
+begin
+  have : 0 ≤ dist x y + dist y x,
+  { rw [←dist_self x],
+    apply dist_triangle },
+  linarith [dist_comm x y]
+end
+
 
 end
 
